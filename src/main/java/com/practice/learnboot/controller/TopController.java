847938a80c763cf8a6729830c8f31be0864bc090
datamoneycar <lichengyuan2018@yahoo.co.jp>
2022-01-26 21:31:48 +0900

    Initial commit

---
+++ src/main/java/com/practice/learnboot/controller/TopController.java
@@ -0,0 +1,14 @@
+package com.practice.learnboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TopController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String test(){
+        return "HelloWorld";
+    }
+}
